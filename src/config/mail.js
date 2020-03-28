516a5c6b2e7f5a6bc731a173bca99d7bccee5608
andre assuncao <andre.assuncao@eyemobile.com.br>
2020-03-27 21:57:29 -0300

    create send email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  sercure: false,
+  auth: {
+    user: 'e34021ed1cc166',
+    pass: '725f81c5f998ce',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
